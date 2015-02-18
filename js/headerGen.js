056b550175f421706297d00b55754b3abafa870d
George Ke <george-ke@hotmail.com>
2015-02-18 00:27:43 -0500

    Gmail logo, non-drag images, magnifying glass

--- js/headerGen.js
+++ js/headerGen.js
@@ -1,10 +1,3 @@
-function setWelcomeHeader() {
-	var msgs = ["Hey!", "Welcome!", "Salutations!", "What's poppin'?"];
-	var rand = Math.floor(Math.random() * msgs.length);
-
-	//document.getElementById("welcome").innerText = msgs[rand];
-}
-
 function setContactHeader() {
 	var msgs = ["Drop me a line!", "Talk to me!", "Let's talk!", "Get linked in!", "Contact me", "Any questions?"];
 	var rand = Math.floor(Math.random() * msgs.length);
