9de7f3069bcead601525a9b1d84314eb9a2d0160
George Ke <georgekeee@gmail.com>
2018-07-24 18:02:12 -0400

    remove contact page

--- js/headerGen.js
+++
@@ -1,6 +0,0 @@
-function setContactHeader() {
-	var msgs = ["Drop me a line!", "Talk to me!", "Let's talk!", "Get linked in!", "Contact me", "Any questions?"];
-	var rand = Math.floor(Math.random() * msgs.length);
-
-	document.getElementById("contact").innerText = msgs[rand];
-}
